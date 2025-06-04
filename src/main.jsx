85809b892c21945e0122e339bcd401c2294a441a
Aleksandar Prodanovic <aleksandar_prod18@hotmail.com>
2025-06-04 08:26:29 +0200

    feat: basic toast functionality with provider and button example

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom/client";
+import { ToastProvider, toast } from "../src";
+
+function App() {
+  return (
+    <div>
+      <button onClick={() => toast({ type: "success", message: "Saved!" })}>
+        Show Success Toast
+      </button>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ToastProvider>
+    <App />
+  </ToastProvider>
+);
